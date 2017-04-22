93eb26c233c07d4a30b6b369ac59e960d79510cc
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-04-22 18:48:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-
+#!/bin/sh
+# instalador como comando de tracebgp
+# Se instalara para poder ser ejecutardo como un comando de Linux
+apt-get install traceroute
+chmod 777 actualizar
+cp actualizar /sbin/
