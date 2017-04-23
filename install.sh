ebe9b632063e86d8943fa5ca1394631ddabaabeb
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-04-23 03:03:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@
 # instalador como comando de tracebgp
 # Se instalara en /sbin/ para poder ser ejecutardo como un comando de Linux
 apt-get install traceroute
+chmod 777 bgp2ip
 chmod 777 tracebgp
+cp bgp2ip /sbin/
 cp tracebgp /sbin/
