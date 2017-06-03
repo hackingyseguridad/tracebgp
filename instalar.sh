d2c3d7dbf1ce3c309136a73006a29a70cbc2ffc3
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-06-03 21:39:16 +0200

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -3,6 +3,8 @@
 # Se instalara en /sbin/ para poder ser ejecutardo como un comando de Linux
 apt-get install traceroute whois
 chmod 777 bgp2ip
+chmod 777 ip2bgp
 chmod 777 tracebgp
 cp bgp2ip /sbin/
+cp ip2bgp /sbin/
 cp tracebgp /sbin/
